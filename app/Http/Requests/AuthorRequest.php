6e1a641d065024ef6b69d5979514c674d5aeb235
quarentyne <peresada1997@gmail.com>
2025-11-09 12:51:34 +0000

    make author migration

---
+++ app/Http/Requests/AuthorRequest.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class AuthorRequest extends FormRequest
+{
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'lastname' => 'required|min:3|string',
+            'firstname' => 'required|min:1|string',
+        ];
+    }
+}
