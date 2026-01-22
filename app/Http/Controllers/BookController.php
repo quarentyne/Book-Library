7cd963247f409324f835743f559b924f7498ccc0
quarentyne <peresada1997@gmail.com>
2026-01-22 22:10:48 +0000

    add book owners

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -9,6 +9,7 @@ use App\Models\Book;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\View\View;
 
 class BookController
@@ -17,7 +18,7 @@ class BookController
     {
         $authors = Author::withSort('lastname', 'ASC')->get();
 
-        $query = Book::query()->with('authors');;
+        $query = Book::query()->with('authors')->with('user');
 
         if($request->get('search')) {
             $query->withSearch($request->get('search'));
--- app/Models/Book.php
+++ app/Models/Book.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Book extends Model
@@ -51,5 +52,10 @@ class Book extends Model
     public function authors(): BelongsToMany
     {
         return $this->belongsToMany(Author::class, 'author_book', 'book_id', 'author_id');
+    }
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'owner_id', 'id');
     }
 }
